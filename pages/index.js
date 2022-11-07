6a7a88d57bf055b973a3df97f7523d2b993e2d7d
ABSanthosh <a.b.santhosh02@gmail.com>
2022-11-07 15:56:36 +0530

    project init

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import Header from "../Components/Header/Header";
+import "../styles/routes/Home.scss";
+
+export default function Home() {
+  return (
+    <div className="LandingPage">
+      <Header />
+    </div>
+  );
+}
