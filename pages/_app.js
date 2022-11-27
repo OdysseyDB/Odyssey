ce71fd8150af23b42b5274c1c9e3fabf553782dc
ABSanthosh <a.b.santhosh02@gmail.com>
2022-11-27 15:53:53 +0530

    feat: added progress bar

--- pages/_app.js
+++ pages/_app.js
@@ -2,20 +2,20 @@ import AuthModal from "../Components/AuthModal/AuthModal";
 import "../styles/root/globals.scss";
 import { AuthProvider, getUserFromSession } from "../context/authContext";
 import App from "next/app";
-
+import NextNProgress from "nextjs-progressbar";
 import Head from "next/head";
 
 function MyApp({ Component, pageProps, user }) {
   return (
-
     <AuthProvider ssrUser={user}>
       <Head>
-      
         <title>Odyssey</title>
         <meta name="description" content="Awesome website for Odyssey" />
-        <link rel="icon" href="/ods_logo.png"/>
+        <link rel="icon" href="/ods_logo.png" />
       </Head>
       <AuthModal />
+      <NextNProgress color="#ff4f46" height={5} />
+
       <Component {...pageProps} />
     </AuthProvider>
   );
